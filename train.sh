1a0a6b60f636081d6aff86261c71ca693ca90b2e
Yuan Zhang <mikezhang95>
2020-05-31 18:58:25 +0800

    bash

--- train.sh
+++ train.sh
@@ -5,9 +5,9 @@ export CUDA_VISIBLE_DEVICES=0
 mode=$1
 dataset=$2
 
-if [ "$mode" == sl ]; then
+if [ $mode == "sl" ]; then
   python -u supervised.py --config_name sl_hdno_$dataset
-elif [ "$mode" == rl ]; then
+elif [ $mode == "rl" ]; then
   python -u reinforce.py --config_name rl_hdno_$dataset
 else
   echo "wrong input"
