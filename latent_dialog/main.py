d86a84e8a6370a6167becbf7b676bc30c0a7cccc
Yuan Zhang <mikezhang95>
2020-05-31 14:07:12 +0800

    fix

--- latent_dialog/main.py
+++ latent_dialog/main.py
@@ -410,10 +410,12 @@ def generate(model, data, config, evaluator, verbose=True, dest_f=None, vec_f=No
                 logger.debug('True: {}'.format(true_str, ))
                 logger.debug('Pred: {}'.format(pred_str, ))
                 logger.debug('-' * 40)
-            dest_f.write('%s-prev_ctx = %s\n' % (keys[b_id], prev_ctx,))
-            dest_f.write('True: {}\n'.format(true_str, ))
-            dest_f.write('Pred: {}\n'.format(pred_str, ))
-            dest_f.write('-' * 40+"\n")
+
+            if dest_f is not None:
+                dest_f.write('%s-prev_ctx = %s\n' % (keys[b_id], prev_ctx,))
+                dest_f.write('True: {}\n'.format(true_str, ))
+                dest_f.write('Pred: {}\n'.format(pred_str, ))
+                dest_f.write('-' * 40+"\n")
 
             if  vec_f is not None:
                 sample = sample_z[b_id]
