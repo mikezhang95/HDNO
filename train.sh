3a5cf53c89c0301e9463dc378d3636f85fa313ea
Jianhong Wang <wangjianhong1993@gmail.com>
2020-06-07 20:29:22 +0100

    update script

--- train.sh
+++ train.sh
@@ -4,12 +4,14 @@ export CUDA_VISIBLE_DEVICES=0
 
 mode=$1
 dataset=$2
-alias=$3
+# alias=$3
 
 if [ $mode == "sl" ]; then
-  python -u supervised.py --config_name sl_hdno_$dataset --alias $alias
+  # python -u supervised.py --config_name sl_hdno_$dataset --alias $alias
+  python -u supervised.py --config_name sl_hdno_$dataset
 elif [ $mode == "rl" ]; then
-  python -u reinforce.py --config_name rl_hdno_$dataset --alias $alias
+  # python -u reinforce.py --config_name rl_hdno_$dataset --alias $alias
+  python -u reinforce.py --config_name rl_hdno_$dataset
 else
   echo "wrong input"
 fi
