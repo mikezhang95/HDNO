d37d7819e12e8be07a1781637bd1c24132a946db
Jianhong Wang <wangjianhong1993@gmail.com>
2020-06-07 22:22:03 +0100

    update bash

--- test.sh
+++ test.sh
@@ -4,14 +4,12 @@ export CUDA_VISIBLE_DEVICES=0
 
 mode=$1
 dataset=$2
-# alias=$3
+beam=${3:-2}
 
 if [ $mode == "sl" ]; then
-  # python -u supervised.py --config_name sl_hdno_$dataset --alias $alias --forward_only --gen_type beam --beam_size $4
-  python -u supervised.py --config_name sl_hdno_$dataset --forward_only --gen_type beam --beam_size $3
+  python -u supervised.py --config_name sl_hdno_$dataset --forward_only --gen_type beam --beam_size $beam
 elif [ $mode == "rl" ]; then
-  # python -u reinforce.py --config_name rl_hdno_$dataset --alias $alias --forward_only --gen_type beam --beam_size $4
-  python -u reinforce.py --config_name rl_hdno_$dataset --forward_only --gen_type beam --beam_size $3
+  python -u reinforce.py --config_name rl_hdno_$dataset --forward_only --gen_type beam --beam_size $beam
 else
   echo "wrong input"
 fi
